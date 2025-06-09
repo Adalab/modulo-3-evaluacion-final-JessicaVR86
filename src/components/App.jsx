37f150d6a294a1d4f32248174228d7246ba337f9
JessicaVR86 <jessica.vld.rdi@gmail.com>
2025-06-09 17:05:04 +0200

    add class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,60 @@
 import "../styles/App.scss";
 
+import { useEffect, useState } from "react";
+
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://hp-api.onrender.com/api/characters/house/gryffindor")
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, []);
+
   return (
     <>
       <header>
-        <h1>Harry Potter</h1>
+        <h1 className="title">Harry Potter</h1>
       </header>
-      <nav>
-        <form>
-          <label htmlFor="filtercharacter"> Busca un personaje: </label>
-          <input type="text" name="filtercharacter" id="filtercharacter" />
-          <label htmlFor="filterhouse">Selecciona la casa:</label>
-          <select name="filterhouse" id="filterhouse">
-            <option value="Gryffindor">Gryffindor</option>
-            <option value="Hufflepuff">Hufflepuff</option>
-            <option value="Ravenclaw">Ravenclaw</option>
-            <option value="Slytherin">Slytherin</option>
-          </select>
+      <nav className="navsearch">
+        <form className="form">
+          <div>
+            <label htmlFor="filtercharacter" className="searchbox">
+              {" "}
+              Busca un personaje:{" "}
+            </label>
+            <input
+              type="text"
+              name="filtercharacter"
+              id="filtercharacter"
+              className="inputclass"
+            />
+          </div>
+          <div>
+            <label htmlFor="filterhouse" className="searchbox">
+              Selecciona la casa:
+            </label>
+            <select name="filterhouse" id="filterhouse" className="inputclass">
+              <option value="Gryffindor">Gryffindor</option>
+              <option value="Hufflepuff">Hufflepuff</option>
+              <option value="Ravenclaw">Ravenclaw</option>
+              <option value="Slytherin">Slytherin</option>
+            </select>
+          </div>
         </form>
       </nav>
 
-      <main>
-        <ul>
-          <li></li>
+      <main className="cardbox">
+        <ul className="cardlist">
+          <li className="card">
+            
+            <figure>
+             
+              <img></img> <figcaption> {movies.name} </figcaption>
+            </figure>
+          </li>
         </ul>
       </main>
     </>
